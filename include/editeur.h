bb4895fcc03923ec26a377612ec4dac1a421becf
Clement-Cauet <70692830+Clement-Cauet@users.noreply.github.com>
2023-12-07 01:37:51 +0100

    Modification editor level

--- include/editeur.h
+++ include/editeur.h
@@ -17,7 +17,9 @@ public:
 	Editeur(sf::RenderWindow* window, int lvl);
 
 	void loadAssets();
+	void loadTexts();
 	int editer();
+	int nbEnnemy(int carte[][NB_BLOCS_HAUTEUR]);
 
 private:
 	sf::RenderWindow* window;
@@ -25,10 +27,13 @@ private:
 	sf::Time frameTime;
 
 	sf::Sprite vide, mur, caisse, objectif, caisseOk, mario;
-	sf::Sprite choixVide, choixMur, choixCaisse, choixObjectif, choixCaisseOk, choixMario;
+	sf::Sprite sprite_overlay, choixVide, choixMur, choixCaisse, choixObjectif, choixCaisseOk, choixMario;
 	sf::Sprite *allAsset[6] = { &vide, &mur, &caisse, &objectif, &caisseOk, &mario };
-	sf::Texture textureVide, textureChoixVide, textureMur, textureChoixMur, textureCaisse, textureChoixCaisse, textureObjectif, textureChoixObjectif, textureCaisseOk, textureChoixCaisseOK, textureMario, textureChoixMario;
+	sf::Texture texture_overlay, textureVide, textureChoixVide, textureMur, textureChoixMur, textureCaisse, textureChoixCaisse, textureObjectif, textureChoixObjectif, textureCaisseOk, textureChoixCaisseOK, textureMario, textureChoixMario;
 	sf::Font font;
+	sf::Text text_command_quit, text_command_save, text_lvl, text_ennemy;
 
-	int lvl;
+	Fichier fichier;
+
+	int lvl, nb_ennemy;
 };
--- src/editeur.cpp
+++ src/editeur.cpp
@@ -4,6 +4,7 @@ Editeur::Editeur(sf::RenderWindow * window, int lvl)
 	: window(window), lvl(lvl)
 {
 	loadAssets();
+	loadTexts();
 	editer();
 }
 
@@ -11,6 +12,13 @@ void Editeur::loadAssets()
 {
 	// Chargement de la police d'écriture
 	font.loadFromFile("./assets/font/CrimsonPro-SemiBold.ttf");
+
+	// Chargement de l'overlay
+	texture_overlay.loadFromFile("./assets/textures/game_overlay.jpg");
+	sprite_overlay.setTexture(texture_overlay);
+	float scale = (LARGEUR_FENETRE) / sprite_overlay.getGlobalBounds().width;
+	sprite_overlay.setScale(scale, scale);
+	sprite_overlay.setPosition(0, 0);
 
 	// Chargement des sprites
 	textureVide.loadFromFile("./assets/textures/vide.png");
@@ -50,14 +58,46 @@ void Editeur::loadAssets()
 	choixMario.setTexture(textureChoixMario);
 }
 
+void Editeur::loadTexts()
+{
+	// Affichage commandes
+	text_command_quit.setFont(font);
+	text_command_quit.setCharacterSize(FONT_SIZE_COMMAND);
+	text_command_quit.setString("- QUIT [ ECHAP ] -");
+	text_command_quit.setPosition(
+		(LARGEUR_FENETRE - text_command_quit.getLocalBounds().width) * 1 / 3,
+		(HAUTEUR_FENETRE - text_command_quit.getLocalBounds().height * 2)
+	);
+
+	text_command_save.setFont(font);
+	text_command_save.setCharacterSize(FONT_SIZE_COMMAND);
+	text_command_save.setString("- SAVE [ ENTER ] -");
+	text_command_save.setPosition(
+		(LARGEUR_FENETRE - text_command_save.getLocalBounds().width) * 2 / 3,
+		(HAUTEUR_FENETRE - text_command_save.getLocalBounds().height * 2)
+	);
+
+	// Affichage du numéro du niveau
+	text_lvl.setFont(font);
+	text_lvl.setCharacterSize(FONT_SIZE_NUMBER);
+	text_lvl.setString(fichier.convertIntToRoman(lvl));
+	text_lvl.setPosition(
+		(LARGEUR_FENETRE - text_lvl.getLocalBounds().width) * 8 / 9,
+		(HAUTEUR_FENETRE - text_lvl.getLocalBounds().height) * 6 / 8
+	);
+
+	// Affichage du nombre d'ennemis
+	text_ennemy.setFont(font);
+	text_ennemy.setCharacterSize(FONT_SIZE_NUMBER);
+}
+
 int Editeur::editer()
 {
 	bool continuer = true;
 	int objetSelect = VIDE;
-	int map[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR] = { 0 };
+	int carte[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR] = { 0 };
 
-	Fichier fichier;
-	fichier.chargerNiveau(map, lvl);
+	fichier.chargerNiveau(carte, lvl);
 
 	// On fait tourner le programme jusqu'à ce que la fenêtre soit fermée
 	while (continuer) {
@@ -94,74 +134,84 @@ int Editeur::editer()
 				}
 				//Sauvegarde et chargement du niveau
 				if (event.key.code == sf::Keyboard::Enter) {
-					fichier.sauvegarderNiveau(map, lvl);
+					fichier.sauvegarderNiveau(carte, lvl);
 				}
 				if (event.key.code == sf::Keyboard::BackSpace) {
-					fichier.chargerNiveau(map, lvl);
+					fichier.chargerNiveau(carte, lvl);
 				}
 			}
-
-			//Placement objet
-			sf::Vector2i mousePosition = sf::Mouse::getPosition(*window);
-			sf::FloatRect Position;
-			for (int ligne = 0; ligne < NB_BLOCS_LARGEUR; ligne++) {
-				for (int colonne = 0; colonne < NB_BLOCS_HAUTEUR; colonne++) {
-					int x = mousePosition.x / TAILLE_BLOC;
-					int y = mousePosition.y / TAILLE_BLOC;
-					Position.top = colonne * TAILLE_BLOC;
-					Position.left = ligne * TAILLE_BLOC;
-					if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-						map[x][y] = objetSelect;
-					}
-					sf::Sprite* asset = allAsset[map[ligne][colonne]];
-					asset->setPosition(Position.left, Position.top);
-
-					window->draw(*asset);
-				}
-			}
-
-			// Affichage du numéro du niveau
-			sf::Text niveau;
-			niveau.setFont(font);
-			niveau.setString("Edition Niveau " + std::to_string(lvl));
-			niveau.setPosition(
-				(LARGEUR_FENETRE_JEU - niveau.getLocalBounds().width) / 2,
-				(HAUTEUR_FENETRE_JEU + niveau.getLocalBounds().height)
-			);
 
 			// Affichage choix des sprites
 			sf::RectangleShape select(sf::Vector2f(68.f, 68.f));
 
-			float largeur = LARGEUR_FENETRE - choixVide.getLocalBounds().width * 1.3;
-			float hauteur = (TAILLE_BLOC / 2) + (choixVide.getLocalBounds().height);
+			float largeur = choixVide.getLocalBounds().width * 1.3;
+			float hauteur = HAUTEUR_FENETRE * 3 / 4;
 
 			select.setFillColor(sf::Color(255, 255, 255, 80));
-			choixVide.setPosition(largeur, hauteur * VIDE);
-			choixMur.setPosition(largeur, hauteur * MUR);
-			choixCaisse.setPosition(largeur, hauteur * CAISSE);
-			choixObjectif.setPosition(largeur, hauteur * OBJECTIF);
-			choixCaisseOk.setPosition(largeur, hauteur * CAISSE_OK);
-			choixMario.setPosition(largeur, hauteur * MARIO);
+			choixVide.setPosition((LARGEUR_FENETRE / 3) + largeur * VIDE, hauteur);
+			choixMur.setPosition((LARGEUR_FENETRE / 3) + largeur * MUR, hauteur);
+			choixCaisse.setPosition((LARGEUR_FENETRE / 3) + largeur * CAISSE, hauteur);
+			choixObjectif.setPosition((LARGEUR_FENETRE / 3) + largeur * OBJECTIF, hauteur);
+			choixCaisseOk.setPosition((LARGEUR_FENETRE / 3) + largeur * CAISSE_OK, hauteur);
+			choixMario.setPosition((LARGEUR_FENETRE / 3) + largeur * MARIO, hauteur);
 			if (objetSelect == VIDE) {
-				select.setPosition(largeur, hauteur * VIDE);
+				select.setPosition((LARGEUR_FENETRE / 3) + largeur * VIDE, hauteur);
 			}
 			if (objetSelect == MUR) {
-				select.setPosition(largeur, hauteur * MUR);
+				select.setPosition((LARGEUR_FENETRE / 3) + largeur * MUR, hauteur);
 			}
 			if (objetSelect == CAISSE) {
-				select.setPosition(largeur, hauteur * CAISSE);
+				select.setPosition((LARGEUR_FENETRE / 3) + largeur * CAISSE, hauteur);
 			}
 			if (objetSelect == OBJECTIF) {
-				select.setPosition(largeur, hauteur * OBJECTIF);
+				select.setPosition((LARGEUR_FENETRE / 3) + largeur * OBJECTIF, hauteur);
 			}
 			if (objetSelect == CAISSE_OK) {
-				select.setPosition(largeur, hauteur * CAISSE_OK);
+				select.setPosition((LARGEUR_FENETRE / 3) + largeur * CAISSE_OK, hauteur);
 			}
 			if (objetSelect == MARIO) {
-				select.setPosition(largeur, hauteur * MARIO);
+				select.setPosition((LARGEUR_FENETRE / 3) + largeur * MARIO, hauteur);
 			}
 
-			window->draw(niveau);
+			window->draw(sprite_overlay);
+			window->draw(text_command_quit);
+			window->draw(text_command_save);
+			window->draw(text_lvl);
+
+			//Placement objet
+			sf::Vector2i mousePosition = sf::Mouse::getPosition(*window);
+			int x = (mousePosition.x - ((LARGEUR_FENETRE - LARGEUR_FENETRE_JEU) / 2)) / TAILLE_BLOC;
+			int y = mousePosition.y / TAILLE_BLOC;
+
+			// Vérifiez si la position de la souris est à l'intérieur des limites de la carte
+			if (x >= 0 && x < NB_BLOCS_LARGEUR && y >= 0 && y < NB_BLOCS_HAUTEUR) {
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+					carte[x][y] = objetSelect;
+				}
+			}
+
+			// Dessinez les objets en fonction de la carte mise à jour
+			for (int ligne = 0; ligne < NB_BLOCS_LARGEUR; ligne++) {
+				for (int colonne = 0; colonne < NB_BLOCS_HAUTEUR; colonne++) {
+					sf::FloatRect Position;
+					Position.top = colonne * TAILLE_BLOC;
+					Position.left = ((LARGEUR_FENETRE)-(LARGEUR_FENETRE_JEU)) / 2 + ligne * TAILLE_BLOC;
+
+					sf::Sprite* asset = allAsset[carte[ligne][colonne]];
+					asset->setPosition(Position.left, Position.top);
+					window->draw(*asset);
+				}
+			}
+
+			nb_ennemy = nbEnnemy(carte);
+
+			text_ennemy.setString(std::to_string(nb_ennemy));
+			text_ennemy.setPosition(
+				(LARGEUR_FENETRE - text_ennemy.getLocalBounds().width) * 1 / 9,
+				(HAUTEUR_FENETRE - text_ennemy.getLocalBounds().height) * 6 / 8
+			);
+			window->draw(text_ennemy);
+
 			window->draw(choixMur);
 			window->draw(choixCaisse);
 			window->draw(choixObjectif);
@@ -173,4 +223,16 @@ int Editeur::editer()
 		}
 		window->clear();
 	}
+}
+
+int Editeur::nbEnnemy(int carte[][NB_BLOCS_HAUTEUR]) {
+	int nb_ennemy = 0;
+	for (int i = 0; i < NB_BLOCS_LARGEUR; ++i) {
+		for (int j = 0; j < NB_BLOCS_HAUTEUR; ++j) {
+			if (carte[i][j] == OBJECTIF) {
+				nb_ennemy++;
+			}
+		}
+	}
+	return nb_ennemy;
 }
